23562799fee5a6d2c5208594306193bc1575f313
Evan Nemerson <evan@nemerson.com>
2017-03-13 21:15:10 -0700

    once, cpu: add CC0 dedication preamble

--- cpu/cpu.c
+++ cpu/cpu.c
@@ -1,3 +1,13 @@
+/* CPU Information (v1)
+ * Portable Snippets - https://gitub.com/nemequ/portable-snippets
+ * Created by Evan Nemerson <evan@nemerson.com>
+ *
+ *   To the extent possible under law, the authors have waived all
+ *   copyright and related or neighboring rights to this code.  For
+ *   details, see the Creative Commons Zero 1.0 Universal license at
+ *   https://creativecommons.org/publicdomain/zero/1.0/
+ */
+
 #include "cpu.h"
 
 #include "../once/once.h"
