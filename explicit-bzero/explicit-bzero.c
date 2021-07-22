6b81724804c543f8ac14c35079f253ea362f5a74
arnavyc <arnavyc@outlook.com>
2021-07-22 14:22:09 +0530

    fixed some of the problems nemequ pointed out

--- explicit-bzero/explicit-bzero.c
+++ explicit-bzero/explicit-bzero.c
@@ -28,12 +28,8 @@
 #include <Windows.h>
 #endif /* _WIN32 */
 
-#ifdef __STDC_LIB_EXT1__
-/* memset_s() function in C11 Annex K (bound-checking interface) */
-#define HAVE_MEMSET_S 1
-#endif /* __STDC_LIB_EXT1__ */
-
-#if (defined(__GLIBC__) && __GLIBC_PREREQ(2, 25)) ||                           \
+#if (defined(__GLIBC__) &&                                                     \
+     (__GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 25))) ||          \
     (defined(__FreeBSD__) && __FreeBSD_version >= 1100037) ||                  \
     (defined(__OpenBSD__) && OpenBSD >= 201405)
 /*
@@ -50,39 +46,20 @@
 #define HAVE_EXPLICIT_MEMSET 1
 #endif
 
-#if defined(__GNUC__) && (defined(__ELF__) || defined(__APPLE_CC__))
-#define HAVE_WEAK_LINKING_SUPPORT 1
-__attribute__((weak)) void dummy_sym_to_avoid_optimization(void *str, size_t n);
-__attribute__((weak)) void dummy_sym_to_avoid_optimization(void *str,
-                                                           size_t n) {
-  (void)str;
-  (void)n;
-}
-#elif !defined(_WIN32)
+#if !defined(HAVE_EXPLICIT_MEMSET) && !defined(HAVE_EXPLICIT_BZERO) && !defined(__STDC_LIB_EXT1__) && !defined (_WIN32)
 static void *(*const volatile memset_ptr)(void *, int, size_t) = memset;
 #endif
 
 void psnip_explicit_bzero(void *str, size_t n) {
 #if defined(HAVE_EXPLICIT_MEMSET)
   (void)explicit_memset(str, 0, n);
-#elif defined(HAVE_MEMSET_S)
+#elif defined(__STDC_LIB_EXT1__)
+  /* memset_s() function in C11 Annex K (bound-checking interface) */
   (void)memset_s(str, (rsize_t)n, 0, (rsize_t)n);
 #elif defined(_WIN32)
   (void)SecureZeroMemory(str, n);
 #elif defined(HAVE_EXPLICIT_BZERO)
   explicit_bzero(str, n);
-#else
-
-#if defined(HAVE_WEAK_LINKING_SUPPORT)
-  /*
-   * Use weak linking if available to prevent compiler from optimizing away
-   * memset calls. (Approach used by libsodium[1]).
-   *
-   * [1]:
-   * https://github.com/jedisct1/libsodium/blob/master/src/libsodium/sodium/utils.c#L106
-   */
-  (void)memset(str, 0, n);
-  dummy_sym_to_avoid_optimization(str, n);
 #else
   /*
    * Use a volatile pointer to memset as used by OpenSSL[1] for securely
@@ -92,7 +69,6 @@ void psnip_explicit_bzero(void *str, size_t n) {
    * https://github.com/openssl/openssl/blob/f77208693ec3bda99618e6f76c0f8d279c0077bb/crypto/mem_clr.c
    */
   (void)(memset_ptr)(str, 0, n);
-#endif /* defined(HAVE_WEAK_LINKING_SUPPORT) */
 
 #ifdef __GNUC__
   /*
